3cdc0f99eacdeff73e70131f3eec89f269c2df13
Egor Zheglov <egorzh97@yandex.ru>
2021-07-15 19:41:48 +0300

    Updates

---
+++ main.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";const e=document.querySelector(".header__info-button");function t(){document.querySelector(".overlay").classList.toggle("overlay_is-active")}document.querySelector(".overlay__close-button").addEventListener("click",t),e.addEventListener("click",t)})();
